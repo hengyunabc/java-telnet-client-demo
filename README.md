e22abd4fde3ba4566045c09dc39d32419a9485da
hengyunabc <hengyunabc@gmail.com>
2018-10-29 18:44:42 +0800

    add demo

---
+++ README.md
@@ -0,0 +1,23 @@
+# java-telnet-client-demo
+
+## Features
+
+* A pure java telnet client demo, connect to remote telnet server.
+
+* Support `Tab`/`Ctrl + C`/`Ctrl + D`
+
+* Using [jline](https://github.com/jline/jline2) and [commons-net](https://commons.apache.org/proper/commons-net/)
+
+
+## How to run
+
+```bash
+mvn clean package
+
+java -jar target/java-telnet-client-demo-0.0.1-SNAPSHOT.jar
+```
+
+You can use this demo connect to [Arthas](https://github.com/alibaba/arthas).
+
+## License
+Apache License V2
---
+++ src/main/java/io/github/hengyunabc/tomcat/TelnetClientDemo.java
@@ -0,0 +1,87 @@
+package io.github.hengyunabc.tomcat;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import org.apache.commons.net.telnet.InvalidTelnetOptionException;
+import org.apache.commons.net.telnet.TelnetClient;
+import org.apache.commons.net.telnet.TelnetOptionHandler;
+import org.apache.commons.net.telnet.WindowSizeOptionHandler;
+
+import jline.Terminal;
+import jline.UnixTerminal;
+import jline.console.ConsoleReader;
+import jline.console.KeyMap;
+
+/**
+ *
+ * @author hengyunabc 2018-10-29
+ *
+ */
+public class TelnetClientDemo {
+	private static final byte CTRL_C = 0x03;
+
+	public static void main(String[] args) throws IOException {
+		final ConsoleReader console = new ConsoleReader(System.in, System.out);
+		console.setHandleUserInterrupt(true);
+		Terminal terminal = console.getTerminal();
+		// support catch ctrl+c event
+		terminal.disableInterruptCharacter();
+		if (terminal instanceof UnixTerminal) {
+			((UnixTerminal) terminal).disableLitteralNextCharacter();
+		}
+
+		final TelnetClient telnet = new TelnetClient();
+		telnet.setConnectTimeout(5000);
+		// send init terminal size
+		TelnetOptionHandler sizeOpt = new WindowSizeOptionHandler(terminal.getWidth(), terminal.getHeight(), true, true,
+				false, false);
+		try {
+			telnet.addOptionHandler(sizeOpt);
+		} catch (InvalidTelnetOptionException e) {
+			// ignore
+		}
+
+		String ip = "127.0.0.1";
+		int port = 3658;
+		if (args.length == 1) {
+			ip = args[0];
+		} else if (args.length >= 2) {
+			ip = args[0];
+			port = Integer.parseInt(args[1]);
+		}
+		try {
+			telnet.connect(ip, port);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// ctrl + c event callback
+		console.getKeys().bind(new Character((char) CTRL_C).toString(), new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					console.getCursorBuffer().clear(); // clear current line
+					telnet.getOutputStream().write(CTRL_C);
+					telnet.getOutputStream().flush();
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+
+		});
+
+		// ctrl + d event call back
+		console.getKeys().bind(new Character(KeyMap.CTRL_D).toString(), new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		IOUtil.readWrite(telnet.getInputStream(), telnet.getOutputStream(), System.in, System.out);
+	}
+
+}
